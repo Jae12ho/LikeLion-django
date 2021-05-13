477d72d6d8cc9fa519754d138c9d6651f66585c0
Jaeho Yang <ho4121e@gmail.com>
2021-05-13 21:27:15 +0900

    secret key hide

--- teamproject/teamproject/settings.py
+++ teamproject/teamproject/settings.py
@@ -21,7 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)8mh-03p!4^%522ahr^t!p!wmfm@_7h-4p=-h()83p&3g+t*+='
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
